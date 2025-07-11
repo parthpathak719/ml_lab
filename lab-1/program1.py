146e34ce3b4c38dc2f416cb1f825bc998f6aafd4
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-07-12 01:25:13 +0530

    yo

--- lab-1/program1.py
+++ lab-1/program1.py
@@ -1,3 +1,11 @@
-print("Hello world")
-print("This is my first program in python")
-print("booo")
+vowels=['a','e','i','o','u']
+count1=0
+count2=0
+string=input("Enter string:")
+for i in string:
+    if(i.lower() in vowels):
+        count1+=1
+    else:
+        count2+=1
+print("Number of vowels:",count1)
+print("Number of consonants:",count2)
