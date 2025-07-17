b366848a9cdc4136539e639d98b50d8db4bdb825
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-07-17 15:05:12 +0530

    hello

--- lab-1/program5.py
+++ lab-1/program5.py
@@ -1,11 +1,20 @@
 import random
 import statistics
 
+def findmean(numbers):
+    mean=statistics.mean(numbers)
+    return mean
+
+def findmedian(numbers):
+    median=statistics.median(numbers)
+    return median
+
+def findmode(numbers):
+    mode=statistics.mode(numbers)
+    return mode
+
 numbers=[random.randint(100,150) for i in range(100)]
-mean=statistics.mean(numbers)
-median=statistics.median(numbers)
-mode=statistics.mode(numbers)
 print("Numbers are:",numbers)
-print("Mean is:",mean)
-print("Median is:",median)
-print("Mode is:",mode)
+print("Mean is:",findmean(numbers))
+print("Median is:",findmedian(numbers))
+print("Mode is:",findmode(numbers))
