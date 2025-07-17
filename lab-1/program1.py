30739aaa25406ef1b62c3c38dae183c0b5ff26ea
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-07-17 14:59:14 +0530

    hello

--- lab-1/program1.py
+++ lab-1/program1.py
@@ -1,11 +1,18 @@
+def findvowels(string,vowels):
+    count1=0
+    for i in string:
+        if(i.lower() in vowels):
+            count1+=1
+    return count1
+
+def findconsonants(string,vowels):
+    count2=0
+    for i in string:
+        if(i.lower() not in vowels):
+            count2+=1
+    return count2
+
 vowels=['a','e','i','o','u']
-count1=0
-count2=0
 string=input("Enter string:")
-for i in string:
-    if(i.lower() in vowels):
-        count1+=1
-    else:
-        count2+=1
-print("Number of vowels:",count1)
-print("Number of consonants:",count2)
+print("Number of vowels:",findvowels(string,vowels))
+print("Number of consonants:",findconsonants(string,vowels))
