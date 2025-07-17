5847b3978a1596937d63e52e452877217f64fda7
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-07-17 15:30:14 +0530

    hello

--- lab-1/program4.py
+++ lab-1/program4.py
@@ -1,19 +1,25 @@
+def createA(a1,a2):
+    A=[]
+    for i in range(a1):
+        row=[]
+        for j in range(a2):
+            a=int(input("Enter element:"))
+            row.append(a)
+        A.append(row)
+    return A
+
+def transpose(A,a1,a2):
+    B=[]
+    for i in range(a1):
+        row1=[]
+        for j in range(a2):
+            b=A[j][i]
+            row1.append(b)
+        B.append(row1)
+    return B
+
 a1=int(input("Enter number of rows:"))
 a2=int(input("Enter number of columns:"))
-A=[]
-for i in range(a1):
-    row=[]
-    for j in range(a2):
-        a=int(input("Enter element:"))
-        row.append(a)
-    A.append(row)
+A=createA(a1,a2)
 print("Matrix A:",A)
-
-B=[]
-for i in range(a1):
-    row1=[]
-    for j in range(a2):
-        b=A[j][i]
-        row1.append(b)
-    B.append(row1)
-print("Transpose of Matrix A:",B)
+print("Transpose of Matrix A:",transpose(A,a1,a2))
