73495a4e445e9209cedc1cff3380aecf604a0299
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-07-17 15:16:53 +0530

    hello

--- lab-1/program3.py
+++ lab-1/program3.py
@@ -1,18 +1,26 @@
+def createA(s1):
+    A=[]
+    for i in range(s1):
+        a=int(input("Enter integer element for A:"))
+        A.append(a)
+    return A
+
+def createB(s2):
+    B=[]
+    for i in range(s2):
+        b=int(input("Enter integer element for B:"))
+        B.append(b)
+    return B
+
+def countcommon(A,B):
+    count=0
+    for i in A:
+        if i in B:
+            count+=1
+    return count
+
 s1=int(input("Enter size of list A:"))
 s2=int(input("Enter size of list B:"))
-A=[]
-B=[]
-for i in range(s1):
-    a=int(input("Enter integer element for A:"))
-    A.append(a)
-for i in range(s2):
-    b=int(input("Enter integer element for B:"))
-    B.append(b)
-print("List A:",A)
-print("List B:",B)
-
-count=0
-for i in A:
-    if i in B:
-        count+=1
-print("Number of common elements:",count)
+A=createA(s1)
+B=createB(s2)
+print("Number of common elements:",countcommon(A,B))
