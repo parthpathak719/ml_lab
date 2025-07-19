7e34ea9af8ead6054a62ff9b87011229e50cace9
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-07-19 19:27:21 +0530

    hello

---
+++ lab-2/program8.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+def load():
+ return pd.read_excel("lab-2/Lab Session Data.xlsx",sheet_name="thyroid0387_UCI")
+
+def impute(df):
+ for col in df.columns:
+  if df[col].isnull().sum()>0:
+   if df[col].dtype=='object':
+    df[col]=df[col].fillna(df[col].mode()[0])
+   else:
+    if np.abs(df[col]-df[col].mean()).max()>3*df[col].std():
+     df[col]=df[col].fillna(df[col].median())
+    else:
+     df[col]=df[col].fillna(df[col].mean())
+ return df
+
+df=load()
+df=impute(df)
+print(df)
